aca8b9ffe0ea9582bb36b283b49f368562e6259c
UntitledFruitCo <glenn@darkmink.com>
2014-06-30 12:03:08 +0100

    phpRecipes

---
+++ Email_with_attachments.php
@@ -0,0 +1,49 @@
+<?php
+$fileatt = $_FILES['uploadfile']['tmp_name']; // Path to the file
+$fileatt_type = "application/octet-stream"; // File Type
+$fileatt_name = $_POST['name']; // Filename that will be used for the file as the attachment
+ 
+$email_from = $_POST['email']; // Who the email is from
+$email_subject = "Email Subject"; // The Subject of the email
+$email_txt = $_POST['message']; // Message that the email has in it
+ 
+$email_to = "contactEmail@example.com"; // Who the email is to
+ 
+$headers = "From: ".$email_from;
+ 
+$file = fopen($fileatt,'rb');
+$data = fread($file,filesize($fileatt));
+fclose($file);
+ 
+$semi_rand = md5(time());
+$mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";
+ 
+$headers .= "\nMIME-Version: 1.0\n" .
+"Content-Type: multipart/mixed;\n" .
+" boundary=\"{$mime_boundary}\"";
+ 
+$email_message .= $_POST['message'] .
+"--{$mime_boundary}\n" .
+"Content-Type:text/html; charset=\"iso-8859-1\"\n" .
+"Content-Transfer-Encoding: 7bit\n\n" .
+$email_message . "\n\n";
+ 
+$data = chunk_split(base64_encode($data));
+ 
+$email_message .= "--{$mime_boundary}\n" .
+"Content-Type: {$fileatt_type};\n" .
+" name=\"{$fileatt_name}\"\n" .
+//"Content-Disposition: attachment;\n" .
+//" filename=\"{$fileatt_name}\"\n" .
+"Content-Transfer-Encoding: base64\n\n" .
+$data . "\n\n" .
+"--{$mime_boundary}--\n";
+ 
+$ok = @mail($email_to, $email_subject, $email_message, $headers);
+ 
+if($ok) {
+echo "<font face=verdana size=2>The file was successfully sent!";
+} else {
+die("Sorry but the email could not be sent. Please go back and try again!");
+}
+?>
